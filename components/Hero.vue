cadf8faa637a0c6d4fa6fd626af68d5026809c81
Guillem <guillem@disedit.com>
2019-10-18 14:23:35 +0200

    Update hero buttons

--- components/Hero.vue
+++ components/Hero.vue
@@ -3,9 +3,13 @@
     <div :class="['hero-motto', `is-on-slide-${currentSlide}`]">
       <span class="hero-motto-title">Acordar</span>
       <span class="hero-motto-subtitle">la política útil</span>
-      <div class="hero-buttons">
-        <b-button variant="dark" size="lg">Programa</b-button>
-        <b-button variant="secondary" size="lg">Vídeo</b-button>
+      <div class="hero-nav">
+        <ul class="hero-nav-items">
+          <li class="item-1"><nuxt-link :to="localePath('programa')">Programa</nuxt-link></li>
+          <li class="item-2"><nuxt-link to="/">Candidatura</nuxt-link></li>
+          <li class="item-3"><nuxt-link to="/">Actua!</nuxt-link></li>
+          <li class="item-4"><nuxt-link to="/">Notícies</nuxt-link></li>
+        </ul>
       </div>
     </div>
     <div class="hero-slides">
@@ -106,20 +110,114 @@
     }
   }
 
-  &-buttons {
-    .btn {
-      margin-right: .5rem;
+  &-nav {
+    position: relative;
+    margin-top: 2rem;
+
+    &-items {
+      display: grid;
+      position: relative;
+      z-index: 2;
+      grid-template-columns: repeat(4, 1fr);
+      list-style: none;
+      margin: 0;
+      padding: 0;
+      grid-gap: 2rem;
+      justify-content: stretch;
+      align-items: center;
+      height: 150px;
+
+      a {
+        position: relative;
+        display: flex;
+        min-height: 100px;
+        font-size: 1.75rem;
+        font-weight: bold;
+        background: $navy;
+        color: $white;
+        border-radius: $border-radius-lg;
+        padding: 1rem 1.5rem;
+        align-items: center;
+        transition: .5s ease-in-out;
+        overflow: hidden;
+        transform: scale(1);
+
+        &:hover {
+          transform: scale(1.1);
+          padding: 2.5rem 1.5rem;
+          text-decoration: none;
+          color: $white;
+
+          &::before {
+            left: 0;
+            bottom: 0;
+          }
+        }
+
+        &::before {
+          content: '';
+          position: absolute;
+          // top: 0;
+          left: -100%;
+          bottom: -100%;
+          width: 100%;
+          height: 100%;
+          background: $teal;
+          z-index: -1;
+          transition: .5s ease-in-out;
+          border-radius: $border-radius-lg;
+        }
+      }
+
+      .item-2 {
+        a::before {
+          background: $orange;
+        }
+      }
+
+      .item-3 {
+        a::before {
+          background: $red;
+        }
+      }
+
+      .item-4 {
+        a::before {
+          background: $yellow;
+        }
+      }
     }
   }
 }
 
-@include media-breakpoint-down(xs) {
+@include media-breakpoint-down(lg) {
+  .hero {
+    &-nav {
+      &-items {
+        grid-gap: 1rem;
+         height: 80px;
+
+        a {
+          min-height: auto;
+          font-size: 1.5rem;
+          padding: 1rem;
+
+          &:hover {
+            padding: 1.25rem 1rem;
+          }
+        }
+      }
+    }
+  }
+}
+
+@include media-breakpoint-down(sm) {
   .hero {
     height: calc(100vh - 3.5rem);
     align-items: flex-end;
 
     &-motto {
-      margin: 1.2rem;
+      margin: 1rem;
       width: 100%;
 
       &-title {
@@ -152,9 +250,31 @@
       }
     }
 
-    &-buttons {
-      margin-top: 3rem;
-      align-self: flex-end;
+    &-nav {
+      &-items {
+        grid-template-columns: repeat(2, 1fr);
+        grid-gap: 0.75rem;
+        height: auto;
+
+        a {
+          min-height: auto;
+          font-size: 1.1rem;
+          border-radius: $border-radius;
+          padding: 0.75rem;
+
+          &:hover {
+            padding: 0.75rem;
+          }
+
+          &:active {
+            transform: scale(1.1);
+          }
+
+          &::before {
+            display: none;
+          }
+        }
+      }
     }
   }
 }
