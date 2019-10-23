5effda82b4b3cd4fd0984243c2fcd598e0d8177f
Carles <carles2612@gmail.com>
2019-10-23 14:03:51 +0200

    Add Candidates page

--- pages/candidatura.vue
+++ pages/candidatura.vue
@@ -1,15 +1,53 @@
 <template>
-  <div>
-    
+  <div class="container-fluid">
+    <candidate :candidate="candidates.joanBaldovi" />
+    <candidate :candidate="candidates.inigoErrejon" />
+    <div class="candidates-provincia">
+      <candidate is-provincia :candidate="candidates.ignasiCandela" />
+      <candidate is-provincia :candidate="candidates.martaSorli" />
+    </div>
   </div>
 </template>
 
 <script>
+  import Candidate from '../components/Candidate'
+  import candidates from './candidatura/candidates'
+
   export default {
-    
+    data () {
+      return {
+        candidates
+      }
+    },
+
+    components: {
+      Candidate,
+    },
+
+    mounted() {
+      console.log(this.candidates)
+    }
   }
 </script>
 
 <style lang="scss" scoped>
+@import '../sass/variables';
 
+.candidates-provincia {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  margin-bottom: 2rem;
+  max-height: 100%;
+  align-items: top;
+}
+
+@include media-breakpoint-down(md) {
+  .candidates-provincia {
+    display: grid;
+    grid-template-columns: 1fr;
+    margin-bottom: 1rem;
+    margin-top: 2.5rem;
+    grid-row-gap: 2.5rem;
+  }
+}
 </style>
