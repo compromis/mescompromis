e43ef69609bbe9a7b40e2196e35085d000c4c002
Guillem <guillem@disedit.com>
2019-10-28 10:59:37 +0100

    Add content for finançament just (val)

--- pages/programa/financament-just.vue
+++ pages/programa/financament-just.vue
@@ -1,12 +1,21 @@
 <template>
-  <div>
-
+  <div class="programa-text">
+    <div v-if="$i18n.locale === 'val'" v-html="contentVal.html"></div>
+    <div v-if="$i18n.locale === 'cas'" v-html="contentCas.html"></div>
   </div>
 </template>
 
 <script>
-  export default {
+  import contentVal from '@/content/val/programa/financament-just/index.md'
+  import contentCas from '@/content/cas/programa/financament-just/index.md'
 
+  export default {
+    data () {
+      return {
+        contentVal,
+        contentCas
+      }
+    }
   }
 </script>
 
