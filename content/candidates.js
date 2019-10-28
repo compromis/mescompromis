2fe8d20659dfe071283bbcf314651edc7915b372
Guillem <guillem@disedit.com>
2019-10-28 10:38:22 +0100

    Add candidate descriptions

---
+++ content/candidates.js
@@ -0,0 +1,83 @@
+import BaldoviImage from '@/assets/images/candidates/joan-baldovi.png'
+import ErrejonImage from '@/assets/images/candidates/inigo-errejon.png'
+import IgnasiImage from '@/assets/images/candidates/ignasi-candela.png'
+import MartaImage from '@/assets/images/candidates/marta-sorli-blau.png'
+
+const candidates = {
+  joanBaldovi: {
+    id: 'joan-baldovi',
+    name: 'Joan Baldoví',
+    image: BaldoviImage,
+    subtitle_val: 'Candidat al Congrés',
+    subtitle_cas: 'Candidato al Congreso',
+    bio_cas: `
+      <p>Soy un maestro de escuela que hace política desde que, siendo adolescente, tomé conciencia de que o los valencianos, los ciudadanos, hacemos política o nos la hacen, como decía decía mi paisano Joan Fuster.</p>
+      <p>Soy suecano y valenciano de nacimiento y de devoción, pero creo que el mundo no se acaba en mi pueblo y mi país. Aprecio mi lengua, la pelota valenciana, nuestras costumbres, nuestros fiestas, nuestra manera peculiar de entender el -complejo- mundo en el que nos ha tocado vivir y creo que la diversidad -toda- nos hace más ricos como personas. Quiero a mi familia, amigos y amigues y no concibo la vida sin ellos.</p>
+      <p>Creo en la utilidad de la política para cambiar las cosas aunque, a menudo, tarden más de lo esperado en llegar. Me gusta escuchar más que hablar y creo que es una buena manera para llegar a acuerdos. Si puedo, siempre soy más partidario de un acuerdo discreto que de un gran desacuerdo.</p>
+      <p>Por último, estoy muy preocupado por el estado del medio que nos envuelve, legado que dejaré a mi nieto y a las nuevas generaciones. Por eso, desde la política y también desde mi vida cotidiana, haré todo lo que sea posible para conservar la maravillosa tierra en la que vivimos y para contribuir a que sea un lugar amable.</p>
+    `,
+    bio_val: `
+      <p>Sóc un mestre d’escola que fa política des que, d’adolescent, vaig prendre consciència de que o els valencians, els ciutadans,  fem política o ens la fan, com deia deia el meu paisà Joan Fuster.</p>
+      <p>Sóc suecà i valencià de naixement i de devoció, però crec que el món no s’acaba en el meu poble i el meu país. Estime la meua llengua, la pilota valenciana, els nostres costums, les nostres festes, la nostra manera peculiar d’entendre el món -complicat- que ens ha tocat viure i crec que la diversitat -tota- ens fa més rics com a persones. Estime la meua família, els meus amics i amigues i no concebisc la vida sense ells.</p>
+      <p>Crec en la utilitat de la política per a canviar les coses, sovint més espaiet del que voldríem. M’agrada escoltar més que parlar i crec que això ajuda als acords. Si puc, sempre sóc més partidari d’un acord discret que d’un desacord gran.</p>
+      <p>Per últim, estic molt preocupat per l’estat del medi que ens envolta i que deixaré al meu nét i a les noves generacions. Per això, des de la política i també des de la meua vida quotidiana, faré tan com sàpiga per conservar la meravellosa terra on vivim y per contribuir a que siga un lloc amable.</p>
+    `,
+    twitter: 'https://twitter.com/joanbaldovi/',
+    facebook: 'https://www.facebook.com/joanbaldovi/',
+    instagram: 'https://www.instagram.com/joanbaldovi/'
+  },
+  inigoErrejon: {
+    id: 'inigo-errejon',
+    name: 'Íñigo Errejón',
+    image: ErrejonImage,
+    subtitle_val: 'Candidat al Congrés',
+    subtitle_cas: 'Candidato al Congreso',
+    bio_val: `
+      <p>Vaig néixer a Madrid en 1983. Des que era molt petit els meus pares em van ensenyar sempre els mateixos valors: un immens afecte per la història democràtica del nostre país i una profunda convicció per la justícia social. Des de l'adolescència he participat en col·lectius socials i polítics, des del “No a la guerra” al moviment estudiantil i les assemblees de barri. Vaig col·laborar amb Joventut Sense Futur durant el 15M, moviment que ens va marcar a molts el camí: la possibilitat de construir una majoria popular nova. Sóc candidat per Més País per a aconseguir en les pròximes eleccions generals un Govern progressista i emprendre els reptes urgents que Espanya necessita.</p>
+    `,
+    bio_cas: `
+      <p>Nací en Madrid en 1983. Desde que era muy pequeño mis padres me enseñaron siempre los mismos valores: un inmenso cariño por la historia democrática de nuestro país y una profunda convicción por la justicia social. Desde la adolescencia he participado en colectivos sociales y políticos, desde el “No a la guerra” al movimiento estudiantil y las asambleas de barrio. Colaboré con Juventud Sin Futuro durante el 15M, movimiento que nos marcó a muchos el camino: la posibilidad de construir una mayoría popular nueva. Soy candidato por Más País para conseguir en las próximas elecciones generales un Gobierno progresista y emprender los retos urgentes que España necesita.</p>
+    `,
+    twitter: 'https://twitter.com/ierrejon',
+    facebook: 'https://www.facebook.com/ierrejon',
+    instagram: 'https://www.instagram.com/ierrejon'
+  },
+  ignasiCandela: {
+    id: 'ignasi-candela',
+    name: 'Ignasi Candela',
+    image: IgnasiImage,
+    subtitle_val: 'Candidat al Congrés per Alacant',
+    subtitle_cas: 'Candidato al Congreso por Alicante',
+    bio_val: `
+      <p>Vaig néixer en 1987 en Crevillent. Ma mare, perruquera, i mon pare, administrador d’una Pime, em transmeteren sempre els valors del respecte, el compromís social i el treball. La meua etapa universitària va estar marcada per la crisi econòmica de 2007 que va truncar les esperances de tota una generació. Des d’aquell moment vaig començar a interessar-me activament per la política com a ferramenta per posar les institucions al servei de les persones. D’esta etapa, vaig aprendre que l’acord era la premissa bàsica per avançar com a societat. Sóc candidat de Compromís - Más País per Alacant per aconseguir un govern progressista que pose a les institucions a treballar pel nostre futur.</p>
+    `,
+    bio_cas: `
+      <p>Nací en 1987 en Crevillent. Mi madre, peluquera, y mi padre, administrador de una Pyme, me transmitieron siempre los valores del respeto, el compromiso social y el trabajo. Mi etapa universitaria estuvo marcada por la crisis económica de 2007 que truncó las esperanzas a toda una generación. Desde ese momento me interesé activamente por la política como medio para poner las instituciones al servicio de las personas. De esta etapa, aprendí que el acuerdo era la premisa básica para avanzar como sociedad. Soy candidato de Más Compromís-Más País por Alicante para conseguir un Gobierno progresista que ponga a las instituciones a trabajar por nuestro futuro.</p>
+    `,
+    twitter: 'https://twitter.com/IgnasiCandela',
+    facebook: 'https://www.facebook.com/IgnasiCandelaSerna',
+    instagram: 'https://www.instagram.com/ignasi_candela_serna/'
+  },
+  martaSorli: {
+    id: 'marta-sorli',
+    name: 'Marta Sorlí',
+    image: MartaImage,
+    subtitle_val: 'Candidata al Congrés per Castelló',
+    subtitle_cas: 'Candidata al Congreso por Castelló',
+    bio_val: `
+      <p>Sóc Marta Sorlí Fresquet, nascuda a Alcalà de Xivert a l’estiu del 1986 a una família agricultora. Entre taronges i mandarines he passat la meua infància, esperant que el meu pare descarregara un camió per poder sopar o despertant-me ma mare amb una telefonada des del seu lloc de feina per anar a l’escola. Vaig decidir estudiar Treball Social amb la voluntat de dedicar la meua vida a les persones més desfavorides. Però la crisi-estafa que hem patit em va portar, als contractes temporals, de becària i en precari.</p>
+      <p>Mai he deixat de formar-me, doncs sóc una més de la generació hiper-preparada. La mediació i els drets humans han sigut les meues especialitzacions, de nou amb voluntat d’esforçar-me per a deixar un món més just a les futures generacions. Veient des del treball social com famílies humils ho perdien tot per culpa de l’especulació, amb amistats a l’estranger i un futur preocupant, vaig començar a implicar-me en política per a capgirar les lleis que protegien els panxes-contentes i desprotegien la ciutadania.</p>
+      <p>Sóc cap de Compromís-Más País per Castelló perquè crec, sincerament, que si volem resultats diferents cal apostar per fer coses profitoses. Castelló mereixem representants que estem a l’altura, acordem i garantim la governabilitat de l’estat. Tal i com fem cada dia a casa nostra, persones a l’altura per treure’ns del bloqueig.</p>
+    `,
+    bio_cas: `
+      <p>Soy Marta Sorlí Fresquet, nacida en Alcalà de Xivert en verano del 1986 en una familia agricultora. Entre naranjas y mandarinas he pasado mi infancia, esperando que mi padre descargara un camión para poder cenar o despertándome con una llamada de teléfono desde su lugar de trabajo de mi madre para ir a la escuela. Decidí estudiar Trabajo Social con la voluntad de dedicar mi vida a las personas más desfavorecidas. Pero la crisis-estafa de la última década, me llevó a encadenar contratos temporales, de becaria y en precario.</p>
+      <p>Nunca he dejado de formarme, pues soy una más de la de la generación híper preparada. La mediación y los derechos humanos han sido mis especializaciones, de nuevo con voluntad de esforzarme para dejar un mundo más justo a las futuras generaciones. Viendo desde el trabajo social como familias humildes lo perdían todo por culpa de la especulación, con amistades en el extranjero y un futuro preocupante, empecé a implicarme en política para cambiar las leyes que protegían los bien alimentados y desprotegen a la ciudadanía.</p>
+      <p>Soy cabeza de Compromís-Más Compromís por Castelló porque creo, sinceramente, que si queremos resultados diferentes hay que apostar por hacer cosas provechosas. Castelló merece representantes que estemos a la altura, acordemos y garanticemos la gobernabilidad del estado. Tal y como hacemos cada día en casa nuestra, personas a la altura para sacarnos del bloqueo.</p>
+    `,
+    twitter: 'https://twitter.com/MartaSorli_',
+    facebook: 'https://www.facebook.com/martasorlifresquet',
+    instagram: 'https://www.instagram.com/martasorli_/'
+  }
+}
+
+export default candidates
